617c8dfc7a7f36a29179ab5e2be211bbfff37ccd
mayrajp <may.musique@gmail.com>
2023-01-07 07:50:05 -0300

    add title section

---
+++ resources/views/site/layouts/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>@yield("title")</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/style_base.css') }}">
+</head>
+
+<body>
+    @yield("content")
+</body>
+
+</html>
